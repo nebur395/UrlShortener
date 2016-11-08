30444920e94b619b32201187ab3d60949f8f9391
Alberto Martinez Menendez <amartinez95zgz@gmail.com>
2016-11-08 12:39:52 +0100

    Dependencies problem solved

--- team/src/main/java/urlshortener/aeternum/web/QrGenerator.java
+++ team/src/main/java/urlshortener/aeternum/web/QrGenerator.java
@@ -3,10 +3,8 @@ package urlshortener.aeternum.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-
-
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.*;
 
 @RequestMapping(value = "/qr", method = RequestMethod.GET)
 public class QrGenerator {
@@ -14,7 +12,7 @@ public class QrGenerator {
     public String generateQR(@RequestParam("URL") String url) {
         Client client = ClientBuilder.newClient();
 
-        javax.ws.rs.core.Response response = client.target("https://chart.googleapis.com/chart?" + url).request().get();
+        Response response = client.target("https://chart.googleapis.com/chart?" + url).request().get();
 
         if(response.getStatus() == 200){
             return "redirect:" + "https://chart.googleapis.com/chart?" + url;
