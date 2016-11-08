55dee3583b6e99d66fbcbc18ef61fbda86e63361
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-11-08 12:45:52 +0100

    Migration to AngularJS: Add infraestructure

---
+++ team/src/main/resources/static/javascript/services.js
@@ -0,0 +1,24 @@
+angular.module('urlShortener')
+
+    // 'urlShortener' service manage the url shortener controller in common
+    .factory('urlShortener', function ($state, $http) {
+
+        return {
+
+            //send the register info to the server
+            shortURL: function (url, callbackSuccess,callbackError) {
+                $http({
+                    method: 'POST',
+                    url: '/link',
+                    data: JSON.stringify(url),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).success(function (data) {
+                    callbackSuccess(data.uri);
+                }).error(function (data) {
+                    callbackError('ERROR');
+                });
+            }
+        };
+    });
