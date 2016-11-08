2c63e4611e2f6d56d40a326a36fa5bbc8626b291
Alberto Martinez Menendez <amartinez95zgz@gmail.com>
2016-11-08 23:30:28 +0100

    Fixed the link to make QR

--- team/src/main/java/urlshortener/aeternum/web/QrGenerator.java
+++ team/src/main/java/urlshortener/aeternum/web/QrGenerator.java
@@ -12,7 +12,7 @@ public class QrGenerator {
     public String generateQR(@RequestParam("URL") String url) {
         Client client = ClientBuilder.newClient();
 
-        Response response = client.target("https://chart.googleapis.com/chart?" + url).request().get();
+        Response response = client.target("https://chart.googleapis.com/chart?cht=qr&chl=" + url).request().get();
 
         if(response.getStatus() == 200){
             return "redirect:" + "https://chart.googleapis.com/chart?" + url;
