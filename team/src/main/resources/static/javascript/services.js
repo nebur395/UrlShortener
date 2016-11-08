460921ebb066f7ab36f5c434dfe79455bb641313
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-11-09 00:16:17 +0100

    Correct serialize problem

--- team/src/main/resources/static/javascript/services.js
+++ team/src/main/resources/static/javascript/services.js
@@ -1,19 +1,18 @@
 angular.module('urlShortener')
 
     // 'urlShortener' service manage the url shortener controller in common
-    .factory('urlShortener', function ($state, $http) {
+    .factory('urlShortener', function ($state, $http, $httpParamSerializer) {
 
         return {
 
             //send the register info to the server
             shortURL: function (url, callbackSuccess,callbackError) {
-                console.log(JSON.stringify(url));
                 $http({
                     method: 'POST',
                     url: '/link',
-                    data: JSON.stringify(url),
+                    data: $httpParamSerializer(url),
                     headers: {
-                        'Content-Type': 'application/json'
+                        'Content-Type': 'application/x-www-form-urlencoded'
                     }
                 }).success(function (data) {
                     callbackSuccess(data.uri);
