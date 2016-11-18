a0aa09d5236d1708dfc20867ddb8d58307c6415b
anicacortes <anagonzalor@gmail.com>
2016-11-18 21:57:15 +0100

    Implementada verificación cada 10 sg de todas las urls

---
+++ team/src/main/java/urlshortener/aeternum/web/ScheduledTask.java
@@ -0,0 +1,75 @@
+package urlshortener.aeternum.web;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import urlshortener.common.repository.ShortURLRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ScheduledTask {
+
+    @Autowired
+    protected ShortURLRepository shortURLRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
+    SafeBrowsing sb;
+    List<String> allUrls;
+
+    /*public ScheduledTask(SafeBrowsing sb) {
+        this.sb = sb;
+    }*/
+
+    public ScheduledTask() {
+    }
+
+    public SafeBrowsing getSb() {
+        return sb;
+    }
+
+    public void setSb(SafeBrowsing sb) {
+        this.sb = sb;
+    }
+
+    public List<String> getAllUrls() {
+        return allUrls;
+    }
+
+    public void setAllUrls(List<String> allUrls) {
+        this.allUrls = allUrls;
+    }
+
+    @Override
+    public String toString() {
+        return "ScheduledTask{" +
+            "allUrls=" + allUrls +
+            '}';
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void checkIsSafe() {
+        //Manera de quitar este sb de aqui? Poniendolo como atributo de la clase lo reconoce como null
+        SafeBrowsing sb = new SafeBrowsing();
+        //Poder pasarle este parametro sin que sea null todo el rato?
+        //allUrls = shortURLRepository.listAllUrls();
+        System.out.println("allurls: " + this.allUrls);
+        if(allUrls != null && !allUrls.isEmpty()) {
+            //System.out.println("Tamaño lista = "+ allUrls.size());
+            //System.out.println(allUrls);
+            for (int i = 0; i <= allUrls.size()-1; i++) {
+                //System.out.println("Compruebo url " + allUrls.get(i));
+                sb.safe(allUrls.get(i));
+            }
+        }
+        else {
+            System.out.println("Lista vacia de urls");
+
+        }
+    }
+
+}
