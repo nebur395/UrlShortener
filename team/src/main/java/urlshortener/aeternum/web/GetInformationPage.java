b528f9871497fcefd63c9662bd67f7f4481df7ad
anicacortes <anagonzalor@gmail.com>
2016-12-05 15:35:11 +0100

    Added the possibility of accesing the unsafe web

--- team/src/main/java/urlshortener/aeternum/web/GetInformationPage.java
+++ team/src/main/java/urlshortener/aeternum/web/GetInformationPage.java
@@ -7,13 +7,18 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import urlshortener.common.domain.ClickTop;
 import urlshortener.common.domain.Matches;
+import urlshortener.common.domain.ShortURL;
 import urlshortener.common.domain.ThreatMatch;
+import urlshortener.common.repository.ClickRepository;
+import urlshortener.common.repository.ShortURLRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -23,6 +28,12 @@ import java.util.List;
 public class GetInformationPage {
     private static final Logger LOG = LoggerFactory.getLogger(GetInformationPage.class);
 
+    @Autowired
+    protected ShortURLRepository shortURLRepository;
+
+    @Autowired
+    protected ClickRepository clickRepository;
+
     @RequestMapping(value = "/unsafePage", method = RequestMethod.GET)
     public ResponseEntity<JSONObject> getInformationPage (HttpServletRequest request) {
         LOG.info("Doing the request to information page...");
@@ -30,24 +41,24 @@ public class GetInformationPage {
         Matches m  = SafeBrowsing.getM();
         List<String> listThreatTypes = new ArrayList<>();
         List<String> listPlatforms = new ArrayList<>();
-        JSONObject jsonObject = new JSONObject();
+        String url = "";
+            JSONObject jsonObject = new JSONObject();
 
         if(m.getMatches() != null) {
             ThreatMatch[] t = m.getMatches();
             for (int i = 0; i < t.length; i++) {
-                //LOG.info("ListaTypes["+i+"] = " + t[i].getThreatType());
-                //LOG.info("ListaPlatform["+i+"] = " + t[i].getPlatformType());
                 if (!listThreatTypes.contains(t[i].getThreatType())) {
                     listThreatTypes.add(t[i].getThreatType());
                 }
                 if (!listPlatforms.contains(t[i].getPlatformType())) {
                     listPlatforms.add(t[i].getPlatformType());
                 }
+                url = t[0].getThreat().getUrl();
             }
 
             jsonObject.put("listThreatTypes", listThreatTypes);
             jsonObject.put("listPlatforms", listPlatforms);
-            //Meter tambien la url corta,buscar en bbdd?
+            jsonObject.put("url", url);
         }
 
         return new ResponseEntity<>(jsonObject, HttpStatus.CREATED);
