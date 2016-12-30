5bd83572fd283a44b6459de765d5d7e9a36dff1b
Alberto Martinez Menendez <amartinez95zgz@gmail.com>
2016-12-30 17:55:57 +0100

    Fixing dependencies.

--- team/src/main/java/urlshortener/aeternum/web/QrGenerator.java
+++ team/src/main/java/urlshortener/aeternum/web/QrGenerator.java
@@ -5,7 +5,7 @@ import com.auth0.jwt.internal.org.apache.commons.codec.binary.Base64;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import com.sun.xml.messaging.saaj.util.ByteOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
